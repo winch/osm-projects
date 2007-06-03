ac6afd440037a19be3032b2330ba7e422ff877cd
david <david@4a3fcead-100c-0410-8434-c0d6b4fee585>
2007-06-03 01:38:27 +0000

    Create indexes for all id columns.
    git-svn-id: http://crab/source/osm@170 4a3fcead-100c-0410-8434-c0d6b4fee585

--- sqlite/database.rb
+++ sqlite/database.rb
@@ -1,12 +1,18 @@
 
 def create_tables(db)
     #node
-    db.execute('CREATE TABLE IF NOT EXISTS node(id INTEGER PRIMARY KEY, lat NUMERIC, lon NUMERIC)')
+    db.execute('CREATE TABLE IF NOT EXISTS node(id NUMERIC, lat NUMERIC, lon NUMERIC)')
+    db.execute('CREATE INDEX node_index on node(id)')
     db.execute('CREATE TABLE IF NOT EXISTS node_tag(id NUMERIC, k TEXT, v TEXT)')
+    db.execute('CREATE INDEX node_tag_index on node_tag(id)')
     #segment
-    db.execute('CREATE TABLE IF NOT EXISTS segment(id INTEGER PRIMARY KEY, node_a NUMERIC, node_b NUMERIC)')
+    db.execute('CREATE TABLE IF NOT EXISTS segment(id NUMERIC, node_a NUMERIC, node_b NUMERIC)')
+    db.execute('CREATE INDEX segment_index on segment(id)')
     db.execute('CREATE TABLE IF NOT EXISTS segment_tag(id NUMERIC, k TEXT, v TEXT)')
+    db.execute('CREATE INDEX segment_tag_index on segment_tag(id)')
     #ways
     db.execute('CREATE TABLE IF NOT EXISTS way(id INTEGER, segment NUMERIC, position NUMERIC)')
+    db.execute('CREATE INDEX way_index on way(id)')
     db.execute('CREATE TABLE IF NOT EXISTS way_tag(id NUMERIC, k TEXT, v TEXT)')
+    db.execute('CREATE INDEX way_tag_index on way_tag(id)')
 end

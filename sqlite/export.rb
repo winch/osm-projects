94f9d466e0454c84f5281e542602a542d983b058
david <david@4a3fcead-100c-0410-8434-c0d6b4fee585>
2007-06-05 19:04:39 +0000

    Add find_node_where method to find nodes matching where_clause
    git-svn-id: http://crab/source/osm@173 4a3fcead-100c-0410-8434-c0d6b4fee585

--- sqlite/export.rb
+++ sqlite/export.rb
@@ -21,21 +21,26 @@ osm_node = Hash.new
 osm_segment = Hash.new
 osm_way = Hash.new
 way_count = 0
+segment_count = 0
+node_count = 0
 
-Find.find_way(db, osm_way, "v = 'Windmill Avenue'")
-Find.find_segment(db, osm_way, osm_segment)
-Find.find_node(db, osm_segment, osm_node)
+#Find.find_way_where(db, osm_way, "v = 'Windmill Avenue'")
+#Find.find_segment(db, osm_way, osm_segment)
+#Find.find_node(db, osm_segment, osm_node)
+Find.find_node_where(db, osm_node, "v = 'post_box'")
 
 #write osm data
 
 #node
 osm_node.each do |id, node|
     Xml.write_node(output, id, node)
+    node_count += 1
 end
 
 #segment
 osm_segment.each do |id, segment|
     Xml.write_segment(output, id, segment)
+    segment_count += 1
 end
 
 #way
@@ -44,7 +49,7 @@ osm_way.each do |id, way|
     way_count += 1
 end
 
-puts "exported #{way_count} way(s)"
+puts "exported #{node_count} nodes, #{segment_count} segments and #{way_count} way(s)"
 
 output.write("</osm>\n")
 output.close
--- sqlite/find.rb
+++ sqlite/find.rb
@@ -1,8 +1,8 @@
 
 module Find
 
-    def Find.find_way(db, osm_way, where_clause)
-        #find ways matching where_clause
+    def Find.find_way_where(db, osm_way, where_clause)
+        #find all ways matching where_clause
         db.execute("select id from way_tag where " + where_clause) do |way|
             way = way[0]
             if osm_way[way].nil?
@@ -22,6 +22,7 @@ module Find
     end
     
     def Find.find_segment(db, osm_way, osm_segment)
+        #find all segments in osm_way
         osm_way.each_value do |way|
             way.segments.each do |segment|
                 db.execute("select node_a, node_b from segment where id = ?", segment) do |nodes|
@@ -42,6 +43,7 @@ module Find
     end
     
     def Find.find_node(db, osm_segment, osm_node)
+        #find all nodes in osm_segment
         osm_segment.each_value do |segment|
             db.execute("select id, lat, lon from node where id = ? or id = ?", segment.node_a, segment.node_b) do |node|
                 if osm_node[node[0]].nil?
@@ -53,6 +55,24 @@ module Find
                     db.execute("select k, v from node_tag where id = ?", node[0]) do |tag|
                         osm_node[node[0]].tags.push(tag)
                     end
+                end
+            end
+        end
+    end
+    
+    def Find.find_node_where(db, osm_node, where_clause)
+        #find all nodes matching where_clause
+        db.execute("select id from node_tag where " + where_clause) do |node|
+            if osm_node[node[0]].nil?
+                osm_node[node[0]] = Node.new
+                #find node attributes
+                db.execute("select lat, lon from node where id = ?", node[0]) do |node_attr|
+                    osm_node[node[0]].lat = node_attr[0]
+                    osm_node[node[0]].lon = node_attr[1]
+                end
+                #add tags
+                db.execute("select k, v from node_tag where id = ?", node[0]) do |tag|
+                    osm_node[node[0]].tags.push(tag)
                 end
             end
         end
